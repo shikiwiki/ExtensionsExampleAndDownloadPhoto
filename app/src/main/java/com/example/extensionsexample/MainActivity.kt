688e053b839a061a3682f0990d17043e2a89584a
ViktoriaAstashonok <122493150+ViktoriaAstashonok@users.noreply.github.com>
2023-12-05 15:06:27 +0300

    "oops" added

--- app/src/main/java/com/example/extensionsexample/MainActivity.kt
+++ app/src/main/java/com/example/extensionsexample/MainActivity.kt
@@ -9,19 +9,28 @@ import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
-    
+    private val mainText: ImageView by lazy { findViewById(R.id.main_text) }
+    private lateinit var inputUrl: EditText
+    private val mainButton: Button by lazy { findViewById(R.id.main_button) }
     private val picture: ImageView by lazy { findViewById(R.id.picture) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        "https://kb.rspca.org.au/wp-content/uploads/2023/06/bearded-dragon-on-sand.jpg"
-            .downloadPictureByUrl()
+        val url: String = "https://kb.rspca.org.au/wp-content/uploads/2023/06/bearded-dragon-on-sand.jpg"
+        url.downloadPictureByUrl()
     }
 
     fun String.downloadPictureByUrl() {
-        Glide.with(this@MainActivity).load(this@downloadPictureByUrl).into(picture)
-
+        if ((this@downloadPictureByUrl.startsWith("http://") || this.startsWith("https://"))
+            && this@downloadPictureByUrl.endsWith(".jpg")) {
+            Glide.with(this@MainActivity)
+                .load(this@downloadPictureByUrl).into(picture)
+        } else {
+            val oopsLogo = "https://cdn.dribbble.com/users/2063970/screenshots/5285981/oops-01_4x.jpg"
+            Glide.with(this@MainActivity)
+                .load(oopsLogo).into(picture)
+        }
     }
 }
 
